22e635cb77afc2bbd6ecceabe55d9e62e8e2d689
Martin Homola <martin.homola@sikmo.cz>
2024-11-21 18:29:10 +0100

    code switcher with php code example

--- dist/theme.js
+++ dist/theme.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";window.theme={globals:{isTouch:"undefined"!=typeof window&&"ontouchstart"in window,isTabActive:!0},init:function(){}},document.addEventListener("DOMContentLoaded",(function(){theme.init()}))})();
+(()=>{"use strict";window.theme={globals:{isTouch:"undefined"!=typeof window&&"ontouchstart"in window,isTabActive:!0},init:function(){alert("yes")}},document.addEventListener("DOMContentLoaded",(function(){theme.init()}))})();
 //# sourceMappingURL=maps/theme.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-import { helpers } 		from './components/helpers.js';
+import { helpers } from './components/helpers.js';
 // import { example } 		from './components/example.js';
 
 window.theme = {
@@ -6,14 +6,14 @@ window.theme = {
 		isTouch: typeof window !== 'undefined' && 'ontouchstart' in window,
 		isTabActive: true,
 	},
-	
+
 	init: function () {
 		var self = this;
-
+		alert("yes");
 		// example.init();
 	},
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    theme.init();
+	theme.init();
 });
